ae851df32f5e30234478a239a511240b770162f9
Oluwatobi Omotayo <tobitech@ymail.com>
2022-07-27 12:48:27 +0100

    Initial Commit

---
+++ LaEvents/LaEventsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaEventsApp.swift
+//  LaEvents
+//
+//  Created by Oluwatobi Omotayo on 27/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct LaEventsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
